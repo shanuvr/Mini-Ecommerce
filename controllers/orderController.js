e3e2d93beb9c4915eaaea2218a54014ab37d5fd9
shanuvrdev <shanuvr112233@gmail.com>
2025-08-11 19:49:16 +0530

    checking before empty array

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,7 +5,7 @@ import orderModel from "../models/orders.js";
 
 
 export const createOrder = async(req,res)=>{
-   console.log(req.session.user);
+    console.log(req.session.user);
     const userId = req.session.user.id
     const findCart = await cartModel.findOne({userId})
     if(!findCart){ return res.json({message:"cart is empty"})}
@@ -14,7 +14,6 @@ export const createOrder = async(req,res)=>{
         let  subtotal=0;
         let  items = [];
         for(let eachItems of findCart.items){
-             console.log("Processing cart item:", eachItems);
             const product = await productModel.findById(eachItems.productId)
             if(!product){continue}
             subtotal = product.productPrice * eachItems.quantity
@@ -27,7 +26,6 @@ export const createOrder = async(req,res)=>{
                 subtotal
             })
         }
-        console.log("Final order items array:", items);
          await orderModel.create({
                 userId,
                 items,
@@ -38,29 +36,5 @@ export const createOrder = async(req,res)=>{
         return res.json({message:"order placed"})
     
 
-}
-
-export const editOrder = async(req,res)=>{
-   try{
-     const id = req.params.id
-    const {deliveryStatus} = req.body
-    const findOrder = await orderModel.findById(id)
-    findOrder.deliveryStatus = deliveryStatus
-    await findOrder.save()
-    return res.json({message:"delevery status Updated"})
-   }catch(err){
-    return res.json({err})
-   }
-}
-
-export const deleteOrder = async(req,res)=>{
-    try{
-         const id = req.params.id
-         const del = await orderModel.findByIdAndDelete(id)
-         res.json({message:"order Deleted Successfully"})
-         
-    }catch(err){
-        res.json({err})
-    }
-
+     
 }
